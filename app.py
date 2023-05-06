8977f4e7e8e0719b86317c726fe681387171eb49
Setup Game <ayoubmzari33@gmail.com>
2023-05-06 12:54:22 +0100

    firefox

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ chrome_options = Options()
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
-
+chrome_options.add_argument("--incognito")
 
 @app.route("/")
 def hello():
