b267bdbfa0520792de34c03d82cdcecfd5ac19ab
Setup Game <ayoubmzari33@gmail.com>
2023-05-06 10:35:57 +0100

    while loop

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask, request
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import threading
 
 app = Flask(__name__)
 
@@ -13,6 +14,39 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  # run in headless mode so no browser window is opened
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36") # set user agent
+
+def scrape_page(url):
+    # Create the Chrome driver instance
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Clear cookies
+    driver.delete_all_cookies()
+    # Try to scrape the page
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    # Find the product title
+    product_title = soup.find("h1", class_="product-title-text")
+    title = product_title.text.strip() if product_title else ""
+
+    # Find all the images inside the "images-view-wrap" class
+    images_view_wrap = soup.find("div", class_="images-view-wrap")
+    images = []
+    if images_view_wrap:
+        for img in images_view_wrap.find_all("img"):
+            src = img.get("src")
+            if "jpg_50x50" in src:
+                src = src.replace("jpg_50x50", "jpg")
+            images.append(src)
+
+    # Close the Chrome driver instance
+    driver.quit()
+
+    return {
+        'images': images,
+        'title': title
+    }
+
 
 @app.route('/')
 def index():
@@ -23,49 +57,24 @@ def index():
 
 @app.route('/scrape')
 def scrape():
-    url = request.args.get('url')
+    urls = request.args.getlist('url')
 
     # Set timeout to 6 minutes (360 seconds)
     socket.setdefaulttimeout(360)
 
-    # Create the Chrome driver instance
-    c = 0
+    threads = []
 
-    while True:
-        driver = webdriver.Chrome(options=chrome_options)
+    for url in urls:
+        t = threading.Thread(target=scrape_page, args=(url,))
+        threads.append(t)
+        t.start()
 
-        # Clear cookies
-        driver.delete_all_cookies()
-        # Try to scrape the page
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
+    for t in threads:
+        t.join()
 
-        # Find the product title
-        product_title = soup.find("h1", class_="product-title-text")
-        title = product_title.text.strip() if product_title else ""
-
-        # Find all the images inside the "images-view-wrap" class
-        images_view_wrap = soup.find("div", class_="images-view-wrap")
-        images = []
-        if images_view_wrap:
-            for img in images_view_wrap.find_all("img"):
-                src = img.get("src")
-                if "jpg_50x50" in src:
-                    src = src.replace("jpg_50x50", "jpg")
-                images.append(src)
-
-        # If the data is not found, wait for a few seconds and try again
-        if not title and not images:
-            print(c)
-            c = c+1
-            time.sleep(5) # Wait for 5 seconds before retrying
-        else:
-            # Close the Chrome driver instance
-            driver.quit()
-            return {
-                'images': images,
-                'title': title
-            }
+    return {
+        'success': True
+    }
 
 
 if __name__ == '__main__':
