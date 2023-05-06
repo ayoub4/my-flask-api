426b710a094ec767f04575fadf0af2867e0fbbfc
Setup Game <ayoubmzari33@gmail.com>
2023-05-06 12:56:21 +0100

    firefox

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-dev-shm-usage") # add this line
 
 @app.route("/")
 def hello():
