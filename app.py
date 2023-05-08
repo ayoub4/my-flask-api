1ce89719a663358188c1585e4b14c98b0933874a
Setup Game <ayoubmzari33@gmail.com>
2023-05-08 18:44:49 +0100

    firefox

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage") # add this line
 chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+chrome_options.add_argument("user-data-dir=/User Data")
 
 if platform.system() == 'Windows':
     chromedriver_path = 'chromedriver.exe'
